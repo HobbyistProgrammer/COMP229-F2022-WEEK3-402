f0f9c8fd009b8a69b658017608d67206d395253e
Benton Le <ble21@my.centennialcollege.ca>
2022-09-23 20:12:40 -0400

    ejs files created

--- app/controllers/index.controller.server.js
+++ app/controllers/index.controller.server.js
@@ -5,5 +5,5 @@ export function HelloWorld(req, res, next){
     // res.setHeader('Content-Type', 'text/plain');
     // res.end('Hello World');
 
-    res.render('index');
+    res.render('index', {title: "home"});
 }
